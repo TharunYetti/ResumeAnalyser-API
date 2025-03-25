92df2ba5e4880d83fbd4b446a8f46fd5dc6f85ab
TharunYetti <tharunyetti24@gmail.com>
2025-03-25 22:31:24 +0530

    Updated the mongodb string

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://rehan_rh__:skMcRioZNosOZ2OD@rehan.l02hu.mongodb.net/resumeAnalyser");
+    await mongoose.connect("mongodb+srv://tharunyetti24:Tharun%40123@cluster0.yzi6k.mongodb.net/ResumeAnalyser?retryWrites=true&w=majority&appName=Cluster0");
 }
 module.exports = connectDB;
