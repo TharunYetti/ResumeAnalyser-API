d135d54e42a62e8b9e93da3912e052fc484132ad
neeraj3000 <neerajboggavarapu098@gmail.com>
2025-03-29 13:54:26 +0530

    small change done in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,121 +3,125 @@ const { validateSignUpData } = require("../utils/validation");
 const authRouter = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
-
 authRouter.post("/signup", async (req, res) => {
-    try {
-     
-      console.log("came to sign up!")
-      validateSignUpData(req);
-      console.log("Validation Successful!");
-      const { firstName, lastName, emailId, password } = req.body;
-      console.log("Parsed user data:", firstName, lastName, emailId, password);
-  
-      const passwordHash = await bcrypt.hash(password, 10);
-  
-      const user = new User({
+  try {
+    console.log("came to sign up!");
+    validateSignUpData(req);
+    console.log("Validation Successful!");
+    const { firstName, lastName, emailId, password } = req.body;
+    console.log("Parsed user data:", firstName, lastName, emailId, password);
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    const token = await jwt.sign(
+      { userId: savedUser._id, email: savedUser.emailId },
+      "RESUME@123",
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: savedUser });
+  } catch (err) {
+    console.error("Error:", err.message);
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId });
+
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      throw new Error("Invalid credentials");
+    }
+    const token = await jwt.sign(
+      { userId: user._id, email: user.emailId },
+      "RESUME@123",
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+authRouter.post("/googlelogin", async (req, res) => {
+  const { emailId, firstName, lastName } = req.body;
+
+  console.log(req.body);
+
+  try {
+    let user = await User.findOne({ emailId });
+
+    if (!user) {
+      console.log("Creating new Google user...");
+      user = new User({
         firstName,
         lastName,
         emailId,
-        password: passwordHash,
+        password: "googleLogin",
       });
-  
-      const savedUser = await user.save();
-
-      const token = await jwt.sign({ userId: savedUser._id, email: savedUser.emailId }, "RESUME@123", {
+      await user.save(); // 🔹 Saves the new user to MongoDB
+    }
+    console.log("User found or created:", user);
+    const token = await jwt.sign(
+      { userId: user._id, email: user.emailId },
+      "RESUME@123",
+      {
         expiresIn: "1d",
-      });
-
-    res.cookie("token", token,{
-        expires: new Date(Date.now() + 8 * 3600000),
-    })
-
-      res.status(201).json({ message: "User registered successfully", data: savedUser });
-    } catch (err) {
-      console.error("Error:", err.message);
-      res.status(400).send("Error: " + err.message);
-    }
-  });
-
-  authRouter.post("/login",async(req, res)=>{
-    try{
-        const {emailId, password} = req.body;
-
-
-        const user = await User.findOne({emailId});
-
-        if(!user){
-            throw new Error("Invalid credentials");
-        }
-        const isPasswordMatched = await bcrypt.compare(password, user.password);
-        if(!isPasswordMatched){
-            throw new Error("Invalid credentials");
-        }
-        const token = await jwt.sign({ userId: user._id, email: user.emailId }, "RESUME@123", {
-            expiresIn: "1d",
-          });
-
-        res.cookie("token", token,{
-            expires: new Date(Date.now() + 8 * 3600000),
-        })
-
-        res.send(user);
-
-    }catch(err){
-        res.status(400).send("Error: "+err.message);
-    }
-  })
-
-
-  authRouter.post('/googlelogin', async (req, res) => {
-    const { emailId,firstName, lastName } = req.body;
-
-
-    console.log(req.body);
-
-    try {
-        
-        let user = await User.findOne({ emailId });
-     
-        if (!user) {
-          console.log("Creating new Google user...");
-          user = await User.create({
-              firstName,
-              lastName,
-              emailId,
-              password: "googleLogin", 
-          });
       }
+    );
 
-        const token = await jwt.sign({ userId: user._id, email: user.emailId }, "RESUME@123", {
-          expiresIn: "1d",
-        });
+    console.log(token);
 
-        console.log(token);
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
 
-      res.cookie("token", token,{
-          expires: new Date(Date.now() + 8 * 3600000),
-      })
-
-      res.send(user);
-
-        
-    } catch (error) {
-        console.error('Error during login:', error);
-        console.log(error);
-        res.status(500).json({ message: 'Server error.' });
-    }
+    res.send(user);
+  } catch (error) {
+    console.error("Error during login:", error);
+    console.log(error);
+    res.status(500).json({ message: "Server error." });
+  }
 });
 
-  
-  authRouter.get("/logout", async (req, res) => {
-    res.cookie("token", null,{
-        expires: new Date(Date.now()),
-    })
-  })
-
+authRouter.get("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+});
 
 module.exports = authRouter;
